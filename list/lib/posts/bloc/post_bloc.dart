7d0002d34253c5f7d8a74fdfcade11627644ecd6
TuanPham <tuan.pham@ad24.cz>
2022-04-04 00:18:03 +0200

    idk

---
+++ list/lib/posts/bloc/post_bloc.dart
@@ -0,0 +1,26 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:http/http.dart' as http;
+
+import '../models/post.dart';
+
+part 'post_event.dart';
+part 'post_state.dart';
+
+class PostBloc extends Bloc<PostEvent, PostState> {
+  PostBloc({required this.httpClient}) : super(const PostState()) {
+    on<PostFetched>(_onPostFetched);
+  }
+
+  final http.Client httpClient;
+}
+
+Future<void> _onPostFetched(PostFetched event, Emitter<PostState> emit) async {
+  if (state.hasReachedMax) return;
+  try {
+    if (state.status == PostStatus.initial) {
+      final posts = await _fetchPost();
+      return emit(state.copyWith)
+    }
+  }
+}
