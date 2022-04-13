fe12ce1c637742adc1f1829cb6950a1518670d3d
Lexa Tuan <tuan.pham@ad24.cz>
2022-04-13 21:37:13 +0200

    bloc modified

--- list/lib/posts/bloc/post_bloc.dart
+++ list/lib/posts/bloc/post_bloc.dart
@@ -2,41 +2,62 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:bloc/bloc.dart';
+import 'package:bloc_concurrency/bloc_concurrency.dart';
 import 'package:equatable/equatable.dart';
 import 'package:http/http.dart' as http;
+import 'package:stream_transform/stream_transform.dart';
 
 import '../models/post.dart';
 
 part 'post_event.dart';
 part 'post_state.dart';
 
+const throttleDuration = Duration(milliseconds: 100);
+
+EventTransformer<E> throttleDroppable<E> (Duration duration) {
+  return (events, mapper) {
+    return droppable<E>().call(events.throttle(duration), mapper);
+  };
+}
+
+
+
 class PostBloc extends Bloc<PostEvent, PostState> {
   PostBloc({required this.httpClient}) : super(const PostState()) {
-    on<PostFetched>(_onPostFetched);
+    on<PostFetched>(
+      _onPostFetched,
+      transformer: throttleDroppable(throttleDuration);
+    );
   }
 
-  Future<void> _onPostFetched(
-      PostFetched event, Emitter<PostState> emit) async {
-    if (state.hasReachedMax) return;
+  final http.Client httpClient;
+
+  Future<void> _onPostFetched(PostEvent event, Emitter<PostState> emit) async{
+  if (state.hasReachedMax) return;
     try {
       if (state.status == PostStatus.initial) {
         final posts = await _fetchPosts();
         return emit(state.copyWith(
           status: PostStatus.success,
-          posts: posts,
-          hasReachedMax: false,
         ));
       }
-      final posts = await _fetchPosts(state.posts.length);
-      emit(posts.isEmpty
-          ? state.copyWith(hasReachedMax: true)
-          : state.copyWith(
-              status: PostStatus.success,
-              posts: List.of(state.posts)..addAll(posts),
-              hasReachedMax: false,
-            ));
-    } catch (_) {
-      emit(state.copyWith(status: PostStatus.failure));
+      final posts = await _fetchPosts 
     }
-  }
+
 }
+
+
+Future<List<Post>> _fetchPosts([int startIndex = 0]) async {
+  final response = await httpClient.get(
+    Uri.https(
+      'jsonplaceholder.typicode.com',
+      '/posts',
+      <String, String> {'_start': '$startIndex', '_limit' : $postLimit}
+  ));
+} 
+
+}
+
+
+
+
